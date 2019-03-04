b3b317672b0ab3a045381da27a8bbe6723c0cef1
Tomasz Swiatek <tomasswiatek@gmail.com>
2019-03-04 23:06:13 +0100

    created cards in card  bookmark

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,8 +2,53 @@
   <div class="team">
     <h1 class="subheading">Team</h1>
     <v-container class="my-5">
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia non fugiat adipisci at voluptate molestiae et, rem quam sapiente quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Non tempore unde nobis excepturi quibusdam ipsam nostrum nemo odit enim provident, eum soluta voluptas quisquam, ut sed impedit veniam rerum! Facere voluptatem cupiditate placeat mollitia molestias nam saepe quos quisquam vero veritatis, repellendus nemo qui minus et architecto possimus? Repudiandae, praesentium.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia non fugiat adipisci at voluptate molestiae et, rem quam sapiente quas!</p>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 flat v-for="mate in team" :key="mate.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">image goes here</v-responsive>
+            <v-card-text>
+              <div class="subheading">{{mate.name}}</div>
+              <div class="teal--text">{{mate.role}}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn flat class="grey--text">
+                <v-icon small left>message</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      team: [
+        {
+          name: "Yennefer",
+          role: "Witch"
+        },
+        {
+          name: "Yarpen",
+          role: "Warrior"
+        },
+        {
+          name: "Jaskier",
+          role: "Songster"
+        },
+        {
+          name: "Zoltan",
+          role: "Leader of the dwarven group"
+        },
+        {
+          name: "Regis",
+          role: "Vampire"
+        }
+      ]
+    };
+  }
+};
+</script>
