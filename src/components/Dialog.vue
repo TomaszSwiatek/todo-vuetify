75ab1fda1bedd089b714eeabf0f294de2984eebc
Tomasz Swiatek <tomasswiatek@gmail.com>
2019-03-06 17:08:48 +0100

    fixed problems wiht vselect and installed firebase

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -6,27 +6,53 @@
     <v-card flat>
       <v-card-title class="headline teal white--text lighten-1">Add new commission:</v-card-title>
       <v-divider></v-divider>
-      <v-card-text class="pa-4">
-        <v-text-field color="teal" label="Title" v-model="commissions.title" prepend-icon="title"></v-text-field>
-        <v-text-field color="teal" label="From" v-model="commissions.person" prepend-icon="people"></v-text-field>
-        <v-select color="teal" :items="availableStatus" label="Status" required prepend-icon="sort"></v-select>
-        <v-menu>
+      <v-form ref="form">
+        <v-card-text class="pa-4">
+          <v-textarea
+            label="Content"
+            color="teal"
+            v-model="commissions.title"
+            prepend-icon="title"
+            :rules="inputRules"
+          ></v-textarea>
+
           <v-text-field
             color="teal"
-            slot="activator"
-            label="Date"
-            v-model="commissions.date"
-            prepend-icon="date_range"
+            label="From"
+            v-model="commissions.person"
+            prepend-icon="people"
+            :rules="inputRules"
           ></v-text-field>
-          <v-date-picker color="teal lighten-2" v-model="commissions.date"></v-date-picker>
-        </v-menu>
-      </v-card-text>
 
-      <v-spacer></v-spacer>
-      <v-card-actions>
+          <v-select
+            color="teal"
+            prepend-icon="sort"
+            :rules="inputRules"
+            item-text="status"
+            item-value="id"
+            :items="select"
+            v-model="commissions.status"
+          ></v-select>
+
+          <v-menu>
+            <v-text-field
+              color="teal"
+              slot="activator"
+              label="Date"
+              v-model="commissions.date"
+              prepend-icon="date_range"
+              :rules="inputRules"
+            ></v-text-field>
+            <v-date-picker color="teal lighten-2" v-model="commissions.date"></v-date-picker>
+          </v-menu>
+        </v-card-text>
+
         <v-spacer></v-spacer>
-        <v-btn color="teal lighten-2" class="mr-4 mb-4" outline flat @click="addCommission">Add</v-btn>
-      </v-card-actions>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="teal lighten-2" class="mr-4 mb-4" outline flat @click="addCommission">Add</v-btn>
+        </v-card-actions>
+      </v-form>
     </v-card>
   </v-dialog>
 </template>
@@ -35,7 +61,26 @@
 export default {
   data() {
     return {
-      availableStatus: ["planned", "ongoing", "overdue"],
+      //validation of forms - at least 3 char. or error message
+      inputRules: [
+        //length is equal or more than 3 oraz show message: 'Type at least 3 characters'
+        v => v.length >= 3 || "Type at least 3 characters"
+      ],
+      //   availableStatus: ["planned", "ongoing", "overdue"],
+      select: [
+        {
+          id: "ongoing",
+          status: "ongoing"
+        },
+        {
+          id: "overdue",
+          status: "overdue"
+        },
+        {
+          id: "planned",
+          status: "planned"
+        }
+      ],
       commissions: [
         {
           title: "",
@@ -48,12 +93,16 @@ export default {
   },
   methods: {
     addCommission() {
-      console.log(
-        this.commissions.title,
-        this.commissions.person,
-        this.commissions.date,
-        this.commissions.status
-      );
+      if (this.$refs.form.validate()) {
+        console.log(
+          this.commissions.title,
+          this.commissions.person,
+          this.commissions.date,
+          this.commissions.status
+        );
+      } else {
+        console.log("submit error");
+      }
     }
   }
 };
